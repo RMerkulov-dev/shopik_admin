306fe7294959e22ffdcd202e011c50f4a9793a30
RMerkulov <fotoromario@gmail.com>
2023-08-16 18:53:49 +0300

    upd

---
+++ app/products/[...edit]/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import {Layout} from "@/app/components";
+
+const EditProductPage = () => {
+    return (
+        <Layout>
+            EditProduct
+        </Layout>
+    );
+};
+
+export default EditProductPage;
+
+
