d2be62ec2e1b3543d74f3bf76cd1dfb3da8d3dd4
RMerkulov <fotoromario@gmail.com>
2023-08-16 14:03:45 +0300

    start add form

--- app/products/new/page.tsx
+++ app/products/new/page.tsx
@@ -2,18 +2,52 @@
 
 import {Layout} from "@/app/components";
 import {Box, Button, TextField, Typography} from "@mui/material";
+import {Controller, FieldValues, useForm} from "react-hook-form";
 
 const AddNewProduct = () => {
+    const {
+        control,
+        handleSubmit,
+        formState: {errors},
+        reset
+    } = useForm({
+        defaultValues: {
+            title: "",
+            description: "",
+            price: 0,
+        }
+    })
+
+    const onSubmit = (data: FieldValues) => {
+        console.log(data)
+        reset()
+    }
+
     return (
         <Layout>
-            <Box component="form" sx={{display: "flex", flexDirection: "column", gap: "20px"}}>
+            <Box component="form" sx={{display: "flex", flexDirection: "column", gap: "20px"}}
+                 onSubmit={handleSubmit(onSubmit)}>
                 <Typography variant="h5">New product</Typography>
-                <TextField id="prod_name" label="Product name" variant="outlined"/>
-                <TextField id="prod_desc" label="Product description" variant="outlined"/>
-                <TextField id="prod_price" type="number" label="Price (in USD)" variant="outlined"/>
+                <Controller name="title" control={control}
+                            render={({field: {onChange, value}}) => (
+                                <TextField id="prod_name" value={value} onChange={onChange} label="Product name"
+                                           variant="outlined"/>
+                            )}
+                />
+                <Controller name="description" control={control}
+                            render={({field: {onChange, value}}) => (
+                                <TextField id="prod_desc" value={value} onChange={onChange} label="Product description"
+                                           variant="outlined"/>
+                            )}
+                />
+                <Controller name="price" control={control}
+                            render={({field: {onChange, value}}) => (
+                                <TextField id="prod_price" value={value} onChange={onChange} type="number"
+                                           label="Price (in USD)" variant="outlined"/>
+                            )}
+                />
                 <Button type="submit" variant={"contained"}>Save</Button>
             </Box>
-
         </Layout>
     );
 };
