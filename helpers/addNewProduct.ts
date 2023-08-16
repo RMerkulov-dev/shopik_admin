5f51745bc38a2aeb2d8a0f6d559d0b68edaab488
RMerkulov <fotoromario@gmail.com>
2023-08-16 17:01:48 +0300

    add new product route

--- helpers/addNewProduct.ts
+++ helpers/addNewProduct.ts
@@ -1,13 +1,17 @@
 import {FieldValues} from "react-hook-form";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export const addNewProduct = async (data: FieldValues) => {
     try {
         const response = await axios.post('/api/add_product', data);
+        toast.success("Product added.")
         return response.data;
     } catch (error) {
+        toast.error("Failed to add product.")
         console.error("Error adding new product:", error);
         throw error;
+
     }
 };
 
